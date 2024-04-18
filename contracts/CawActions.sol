8b7321abec41cdfe04ccfec5b58754d9f51e97ba
Gilgamesh.Caw <Gilgamesh.Caw@proton.me>
2024-04-18 21:38:34 +0700

    clean up

--- contracts/CawActions.sol
+++ contracts/CawActions.sol
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/utils/Context.sol";
-import "../node_modules/@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "./interfaces/ISpend.sol";
 
 contract CawActions is Context {
--- contracts/CawName.sol
+++ contracts/CawName.sol
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "../node_modules/@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./CawNameURI.sol";
 
 // AccessControlEnumerable,
