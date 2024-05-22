c07e3caf7e12c74fc419815f26b9020212141924
Gilgamesh.Caw <Gilgamesh.Caw@proton.me>
2024-05-22 12:35:02 +0700

    Added test contract

---
+++ contracts/Test.sol
@@ -0,0 +1,11 @@
+// contracts/CawName.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract Test {
+
+  function test() public view returns (uint256) {
+    return 666;
+  }
+}
